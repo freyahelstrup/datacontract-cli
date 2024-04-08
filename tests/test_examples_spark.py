15c5278b5bd7e2e72e725c3ac0e673e40b3534d3
Freya Gerup Helstrup <freya.helstrup@ufst.dk>
2024-04-08 10:13:15 +0200

    prepare_df function

--- tests/test_examples_spark.py
+++ tests/test_examples_spark.py
@@ -16,15 +16,7 @@ def test_examples_spark():
     spark = SparkSession.builder.appName("Testing PySpark Data Contract").getOrCreate()
     
     # Read data
-    path = "examples/spark/data/orders-1.json"
-    schema = StructType([
-      StructField("order_id", StringType(), True),
-      StructField("order_timestamp", TimestampType(), True),
-      StructField("order_total", LongType(), True),
-      StructField("customer_id", StringType(), True),
-      StructField("customer_email_address", StringType(), True)]
-    )
-    df = spark.read.schema(schema).json(path)
+    df = _prepare_df(spark)
 
     # Create view
     df.createOrReplaceTempView(dataset_name)
@@ -36,3 +28,15 @@ def test_examples_spark():
     print(run)
     assert run.result == "passed"
     assert all(check.result == "passed" for check in run.checks)
+
+def _prepare_df(spark):
+    path = "examples/spark/data/orders-1.json"
+    schema = StructType([
+      StructField("order_id", StringType(), True),
+      StructField("order_timestamp", TimestampType(), True),
+      StructField("order_total", LongType(), True),
+      StructField("customer_id", StringType(), True),
+      StructField("customer_email_address", StringType(), True)]
+    )
+    df = spark.read.schema(schema).json(path)
+    return df
